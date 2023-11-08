122620700b518342b396e996fa9e86dd619b096f
BrunoBuona <brunobuonassisa@gmail.com>
2023-11-08 12:31:00 -0300

    1.0.19

--- src/pages/NewEvent/NewEvent.jsx
+++ src/pages/NewEvent/NewEvent.jsx
@@ -1,11 +1,15 @@
 import AdminTopNavbar from "../../layouts/Admin/AdminTopNavbar/AdminTopNavbar"
-import { useRef } from "react"
 import './NewEvent.css'
+import { useRef } from "react"
+import axios from "axios"
+import {BASE_URL} from '../../api/url'
 
 export default function NewEvent() {
-    const clientRef = useRef()
+    const {token} = JSON.parse(localStorage.getItem('token'))
+    const clientRef = useRef("")
     const dateRef = useRef()
-    const timeRef = useRef()
+    const timeArrivalRef = useRef()
+    const timeEventRef = useRef()
     const placeRef = useRef()
     const categoryRef = useRef()
     const capacityRef = useRef()
@@ -15,21 +19,27 @@ export default function NewEvent() {
     const animatorsRef = useRef()
     const extrasRef = useRef()
 
-    const data = {
-        client: '',
-        date: '',
-        time: '',
-        place: '',
-        category: '',
-        capacity: '',
-        organizer: '',
-        phone: '',
-        email: '',
-        animators: [],
-        extras: [],
+    const createEvent = async () => {
+        const eventData = {
+            cliente: clientRef.current.value,
+        };
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}` // Agregar el token como Bearer
+            }
+        };
+
+        try {
+            const response = await axios.post(`${BASE_URL}/api/events/`, eventData, config);
+            alert("Evento creado exitosamente", response.data);
+        } catch (error) {
+            alert("Error al crear el evento");
+            console.log(error);
+        }
     }
 
-    return(
+    return (
         <>
             <AdminTopNavbar
                 otherLinks={[
@@ -39,77 +49,77 @@ export default function NewEvent() {
                     // Agrega más enlaces de otras categorías según sea necesario
                 ]}
             />
-        <main className="ce-main-container">
-            <div className="ce-main-titles">
-                <h1>CREAR NUEVO EVENTO</h1>
-            </div>
-            <div className="ce-main-form">
-                <div className="ce-main-form-container">
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="title">Nombre del Cliente</label>
-                        <input type="text" name="title" id="title" />
+            <main className="ce-main-container">
+                <div className="ce-main-titles">
+                    <h1>CREAR NUEVO EVENTO</h1>
+                </div>
+                <div className="ce-main-form">
+                    <div className="ce-main-form-container">
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="client">Nombre del Cliente</label>
+                            <input ref={clientRef} type="text" name="client" id="client" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="date">Fecha</label>
+                            <input ref={dateRef} type="date" name="date" id="date" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="timeArrival">Hora de Llegada</label>
+                            <input ref={timeArrivalRef} type="time" name="timeArrival" id="timeArrival" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="timeEvent">Hora del Evento</label>
+                            <input ref={timeEventRef} type="time" name="timeEvent" id="timeEvent" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="place">Lugar</label>
+                            <input ref={placeRef} type="text" name="place" id="place" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="category">Tipo de evento</label>
+                            <select ref={categoryRef} name="category" id="category">
+                                <option value="1">Seleccionar</option>
+                                <option value="1">Cumpleaños Niños</option>
+                                <option value="3">Cumpleaños XV</option>
+                                <option value="4">Casamiento</option>
+                            </select>
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="extras">Adicionales (Varían según tipo de evento)</label>
+                            <select ref={extrasRef} name="extras" id="extras">
+                                <option value="1">Seleccionar</option>
+                                <option value="1">Inflable</option>
+                                <option value="2">Celebridad</option>
+                                <option value="3">Catering</option>
+                                <option value="4">Fotografía</option>
+                                <option value="5">A domicilio</option>
+                            </select>
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="capacity">Cantidad Invitados</label>
+                            <input ref={capacityRef} type="number" name="capacity" id="capacity" placeholder="En números, indica la cantidad de invitados del evento." />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="animators">Asignar Animadores</label>
+                            <input ref={animatorsRef} type="search" placeholder="Escribe el nombre del animador." />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="organizer">Salón</label>
+                            <input ref={organizerRef} type="text" name="organizer" id="organizer" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="phone">Teléfono</label>
+                            <input ref={phoneRef} type="text" name="phone" id="phone" />
+                        </div>
+                        <div className="ce-main-form-container-input">
+                            <label htmlFor="email">Email</label>
+                            <input ref={emailRef} type="email" name="email" id="email" />
+                        </div>
+                        <h6 style={{ color: 'grey' }}>Los datos dinámicos no están disponibles durante la BETA.</h6>
+                        <button className="btn-confirm-ne" onClick={createEvent}>Crear nuevo evento</button>
                     </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="date">Fecha</label>
-                        <input type="date" name="date" id="date" />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="time">Hora de Llegada</label>
-                        <input type="time" name="time" id="time" />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="time">Hora del Evento</label>
-                        <input type="time" name="time" id="time" />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="place">Lugar</label>
-                        <input type="text" name="place" id="place" />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="price">Tipo de evento </label>
-                        <select name="category" id="category" >
-                            <option value="1">Seleccionar</option>
-                            <option value="1">Cumpleaños Niños</option>
-                            <option value="3" onSelect={e => setEventPrice(30)}>Cumpleaños XV</option>
-                            <option value="4">Casamiento</option>
-                        </select>
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="category">Adicionales  (Varian segun tipo de evento) </label>
-                        <select name="category" id="category" >
-                            <option value="1">Seleccionar</option>
-                            <option value="1">Inflable</option>
-                            <option value="2">Celebridad</option>
-                            <option value="3">Catering</option>
-                            <option value="4">Fotografia</option>
-                            <option value="5">A domicilio</option>
-                        </select>
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="capacity">Cantidad Invitados</label>
-                        <input type="number" name="capacity" id="capacity" placeholder="En numeros indicá la cantidad de invitados del evento." />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="capacity">Asignar Animadores</label>
-                            <input type="search" placeholder="Escribi el nombre del animador." />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="organizer">Salon</label>
-                        <input type="text" name="organizer" id="organizer" />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="phone">Teléfono</label>
-                        <input type="text" name="phone" id="phone" />
-                    </div>
-                    <div className="ce-main-form-container-input">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="email" />
-                    </div>
-                    <h6 style={{color:'grey'}}>Los datos dinamicos no estan disponibles durante la BETA.</h6>
-                    <button className="btn-confirm-ne">Crear nuevo evento</button>
-                    </div>
-                    </div>
-        </main>
+                </div>
+            </main>
         </>
     )
 }
